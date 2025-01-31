294ac6d210433dad76b7d1fb899cb936a6d20e8c
Mihir911 <167846524+Mihir911@users.noreply.github.com>
2025-01-31 22:18:03 +0530

    Update script.js

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,30 +1,19 @@
-const input = document.getElementById('inputtext');
-const buttons = document.querySelectorAll('button');
+const display = document.getElementById("display");
+
+function appendToDisplay(input) {
+    display.value += input
+};
+
+function clearDisplay() {
+    display.value = "";
+};
+
+
 
 function calculate(expression) {
-  console.log(expression);
-  try {
-    return new Function('return ' + expression)();
-  } catch (error) {
-    return 'Malformed Operation';
-  }
+        try {
+            display.value = eval(display.value);
+        } catch (error) {
+            display.value = "Error"
+        }
 }
-
-function operation(buttonValue) {
-  if (buttonValue === 'C') {
-    input.value = '';
-  } else if (buttonValue === 'DEL') {
-    input.value = input.value.slice(0, -1);
-  } else if (buttonValue === '=') {
-    input.value = calculate(input.value);
-  } else {
-    input.value += buttonValue;
-  }
-}
-
-buttons.forEach(button => {
-  let buttonValue = button.innerText;
-  button.addEventListener('click', function () {
-    operation(buttonValue);
-  });
-});
